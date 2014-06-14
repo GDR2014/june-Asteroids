cc45ebe7bf41d8861102d5d9389dd27406063340
KHReenberg <khreenberg@gmail.com>
2014-06-14 17:19:00 +0200

    Begins work on the player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float thrusterForce = 300;
+    public float turnSpeed = 7.5f;
+
+    void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	    var turn = Input.GetAxis( "Horizontal" ) * turnSpeed * -1;
+        transform.Rotate( new Vector3(0,0,turn) );
+	}
+}
