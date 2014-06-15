e7425bf3fc39d9a8ad024a15c26cf2ba3f074a7d
KHReenberg <khreenberg@gmail.com>
2014-06-15 21:42:10 +0200

    Creates proper spawning and property scaling of asteroids

--- Assets/Scripts/Managers/WaveManager.cs
+++ Assets/Scripts/Managers/WaveManager.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class WaveManager : MonoBehaviour {
 
@@ -14,6 +17,9 @@ public class WaveManager : MonoBehaviour {
         }
     }
 
+    void Start() {
+        StartCoroutine( LoadNextWave() );
+    }
 
     public void CheckForAsteroids() {
         bool asteroidsLeft = FindObjectOfType<Asteroid>() != null;
@@ -29,7 +35,27 @@ public class WaveManager : MonoBehaviour {
     }
 
     void SpawnAsteroids() {
-        
+        foreach( var spawnPoint in GetAsteroidSpawnPoints() ) {
+            var asteroid = PrefabManager.Instance.Wrapsteroid.Spawn( spawnPoint );
+            asteroid.RemainingSplits = asteroid.baseSplits;
+        }
+    }
+
+    IEnumerable<Vector2> GetAsteroidSpawnPoints() {
+        Vector2[] points = new Vector2[wave];
+        var cam = Camera.main;
+        var height = cam.orthographicSize;
+        var width = cam.aspect * height;
+        var player = FindObjectOfType<Player>();
+        var pos = player.transform.position;
+        var x = width / 4 * 3;
+        var y = height / 4 * 3;
+        var xSign = pos.x < 0 ? 1 : -1;
+        var ySign = pos.y < 0 ? 1 : -1;
+        for( int i = 0; i < points.Length; i++ ) {
+            points[i] = new Vector2(Random.value * x * xSign, Random.value * y * ySign);
+        }
+        return points;
     }
 
 }
