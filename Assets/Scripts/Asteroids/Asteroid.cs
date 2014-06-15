e19145ac2c0a581f72400a36d6984d187dbeb066
KHReenberg <khreenberg@gmail.com>
2014-06-15 19:49:40 +0200

    Nudges asteroids after splitting to help resolve collsions

--- Assets/Scripts/Asteroids/Asteroid.cs
+++ Assets/Scripts/Asteroids/Asteroid.cs
@@ -7,6 +7,7 @@ public class Asteroid : MonoBehaviour {
     public int remainingSplits = 2;
 
     public float baseMass = 10f;
+    public float splitNudgePower = 1;
 
     public Vector2 scale {
         get { return wrapper.ghostScale; }
@@ -36,11 +37,15 @@ public class Asteroid : MonoBehaviour {
             fragment.scale = scale / fragments.Length;
             fragment.remainingSplits = remainingSplits - 1;
         }
+        Vector2 fragPos = fragments[0].transform.position;
+        fragPos += Random.insideUnitCircle * splitNudgePower * .001f;
+        fragments[0].transform.position = fragPos;
     }
 
     void Die() {
         if( remainingSplits > 0 ) Split();
         this.Recycle();
+        WaveManager.Instance.CheckForAsteroids();
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
---
+++ Assets/Scripts/Managers/WaveManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour {
+
+    public int wave = 0;
+    public float interWaveDelay = 1f;
+
+    private static WaveManager _instance;
+    public static WaveManager Instance {
+        get {
+            if( _instance == null ) _instance = FindObjectOfType<WaveManager>();
+            return _instance;
+        }
+    }
+
+
+    public void CheckForAsteroids() {
+        bool asteroidsLeft = FindObjectOfType<Asteroid>() != null;
+        Debug.Log( "Asteroids left: " + asteroidsLeft );
+        if( asteroidsLeft ) return;
+        StartCoroutine( LoadNextWave() );
+    }
+
+    IEnumerator LoadNextWave() {
+        wave++;
+        yield return new WaitForSeconds( interWaveDelay );
+        SpawnAsteroids();
+    }
+
+    void SpawnAsteroids() {
+        
+    }
+
+}
