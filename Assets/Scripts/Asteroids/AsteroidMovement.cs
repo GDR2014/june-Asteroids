0d36708d6a375d0a36685c3876aa4b3fbd2d9e26
KHReenberg <khreenberg@gmail.com>
2014-06-15 12:49:36 +0200

    Adds rotation to individual asteroids in a wrapsteroid

---
+++ Assets/Scripts/Asteroids/AsteroidMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+[RequireComponent(typeof(Wrapper))]
+public class AsteroidMovement : MonoBehaviour {
+
+    public float maxRandomRotationSpeed;
+    public float rotationSpeed;
+    private ReadOnlyCollection<Transform> ghosts;
+
+    void OnEnable() {
+        ghosts = GetComponent<Wrapper>().Ghosts;
+        rotationSpeed = Random.value * maxRandomRotationSpeed - maxRandomRotationSpeed / 2;
+        StartCoroutine( rotationRoutine() );
+    }
+
+    void OnDisable() {
+        StopCoroutine( rotationRoutine() );
+    }
+
+    IEnumerator rotationRoutine() {
+        while( true ) {
+            foreach( var ghost in ghosts ) {
+                ghost.Rotate( new Vector3(0,0,rotationSpeed) );
+            }
+            yield return null;
+        }
+    }
+}
