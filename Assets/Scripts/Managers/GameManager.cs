eadba197c439b913711ec5b8ff67d262691da02c
KHReenberg <khreenberg@gmail.com>
2014-06-16 11:13:17 +0200

    Adds lives and invincibility
    - Also fixes gun bug

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,9 +2,20 @@
 
 public class GameManager : MonoBehaviour {
 
+    private static GameManager _instance;
+    public static GameManager Instance {
+        get {
+            if( _instance == null ) _instance = FindObjectOfType<GameManager>();
+            return _instance;
+        }
+    }
+
     void Awake() {
         // Ignore collisions between player and player bullets.
         Physics2D.IgnoreLayerCollision( 8, 9 );
     }
 
+    public void GameOver() {
+        Debug.Log("Game over! :(");
+    }
 }
